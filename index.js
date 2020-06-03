620f46009628a28a2cfdc2e8aa159b94ed3f5cd2
Jeff Jiang <jeffjiang8@gmail.com>
2020-06-03 16:58:21 -0400

    2.0

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ function chunk(array, size) {
     }
 
     return chunked
+
+    // or
+    
+    // const chunked = []
+
+    // for ( let element of array ) {
+    //     const last = chunked[chunked.length - 1]
+
+    //     if ( !last || last.length === size) {
+    //         chunked.push([element])
+    //     } else {
+    //         last.push(element)
+    //     }
+    // }
+
+    // return chunked
 }
